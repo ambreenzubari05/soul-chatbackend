e4c139e2adf4f41b3f84f3f93c98fa0eeaf575e2
ambreenzubari <ambreenzubari3ss@gmail.com>
2023-11-14 01:40:50 +0500

    add changes

--- index.js
+++ index.js
@@ -1,42 +1,119 @@
+// const path = require('path');
+// const { createServer } = require('http');
+// const app=require('express')();
+// const express = require('express');
+// const { getIO, initIO } = require('./socket');
+// const cors = require('cors');
+// const { Socket } = require('socket.io');
+// const http = require('http').Server(app);
+// // const app = express();
+// app.use(cors()); // Enable CORS for all routes
+// var io=require('socket.io')(http);
+// // app.use('/', express.static(path.join(__dirname, 'static')));
+// app.use('/',function(req, res, next){
+//     var options={
+//         root:path.join(__dirname)
+//     }
+//     var filename='index.html'
+//     res.sendFile(filename, options)
+// })
+// io.on('connection', function(){
+//     console.log("user connected")
+//     Socket.on('disconnect', function(){
+//         console.log("user disconnected")
+//     })
+// })
+
+// const httpServer = createServer(app);
+
+// let port = process.env.PORT || 3500;
+
+// try{
+//     initIO(httpServer);
+//     console.log("Succ")
+
+// }
+// catch{
+//     console.log("Errconst path = require('path');
+// const { createServer } = require('http');
+// const express = require('express');
+// const cors = require('cors');
+// const { initIO } = require('./socket');
+// const admin = require('firebase-admin');
+
+// const app = express();
+// const httpServer = createServer(app);
+
+// // Initialize Firebase Admin SDK
+// const serviceAccount = require('path/to/your/serviceAccountKey.json');
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// app.use(cors());
+
+// app.use('/', function (req, res, next) {
+//   var options = {
+//     root: path.join(__dirname)
+//   };
+//   var filename = 'index.html';
+//   res.sendFile(filename, options);
+// });
+
+// try {
+//   initIO(httpServer, admin);
+//   console.log("Socket.IO initialized successfully");
+// } catch (error) {
+//   console.error("Error initializing Socket.IO:", error);
+// }
+
+// const port = process.env.PORT || 3500;
+
+// httpServer.listen(port, () => {
+//   console.log(`Server started on port ${port}`);
+// });
+// or")
+// }
+// httpServer.listen(port)
+// console.log("Server started on ", port);
+
+// getIO();
+
 const path = require('path');
 const { createServer } = require('http');
-const app=require('express')();
 const express = require('express');
-const { getIO, initIO } = require('./socket');
 const cors = require('cors');
-const { Socket } = require('socket.io');
-const http = require('http').Server(app);
-// const app = express();
-app.use(cors()); // Enable CORS for all routes
-var io=require('socket.io')(http);
-// app.use('/', express.static(path.join(__dirname, 'static')));
-app.use('/',function(req, res, next){
-    var options={
-        root:path.join(__dirname)
-    }
-    var filename='index.html'
-    res.sendFile(filename, options)
-})
-io.on('connection', function(){
-    console.log("user connected")
-    Socket.on('disconnect', function(){
-        console.log("user disconnected")
-    })
-})
+const { initIO } = require('./socket');
+const admin = require('firebase-admin');
+const config = require("config");
 
+const app = express();
 const httpServer = createServer(app);
 
-let port = process.env.PORT || 3500;
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./path/soulchat-33fdf-firebase-adminsdk-52upo-e660863abf.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-try{
-    initIO(httpServer);
-    console.log("Succ")
+app.use(cors());
+app.use('/', function (req, res, next) {
+  var options = {
+    root: path.join(__dirname)
+  };
+  var filename = 'index.html';
+  res.sendFile(filename, options);
+});
 
+try {
+  initIO(httpServer, admin);
+  console.log("Socket.IO initialized successfully");
+} catch (error) {
+  console.error("Error initializing Socket.IO:", error);
 }
-catch{
-    console.log("Error")
-}
-httpServer.listen(port)
-console.log("Server started on ", port);
 
-getIO();
+const port = process.env.PORT || 3500;
+
+httpServer.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
--- socket.js
+++ socket.js
@@ -1,41 +1,59 @@
 const { Server } = require("socket.io");
+const admin = require('firebase-admin');
+
 let IO;
 
-module.exports.initIO = (httpServer) => {
+
+module.exports.initIO = (httpServer, admin) => {
   IO = new Server(httpServer);
- 
-  console.log("initIOlllllllll-=------11");
+
+  console.log("Socket.IO initialized");
+  // IO = new Server(httpServer);
+
+  console.log("Socket.IO initialized");
 
   IO.use((socket, next) => {
-    console.log("initIOlllllllll-=------");
-    if (socket.handshake.query) {
-      let callerId = socket.handshake.query.callerId;
+    console.log("initIOlllllllll-=------",socket.handshake);
+    if (socket.handshake.auth.token) {
+      let callerId = socket.handshake.auth.token; // Check if this is a valid Firebase ID token
       socket.user = callerId;
-      next();
+      verifyFirebaseToken(callerId)
+        .then(() => {
+          next();
+        })
+        .catch((error) => {
+          console.error('Firebase authentication error:', error);
+          next(new Error('Firebase authentication error'));
+        });
+    } else {
+      console.error('Firebase authentication error: No token provided.');
+      next(new Error('Firebase authentication error: No token provided.'));
     }
-  }); 
+  });
+  
+
   IO.on("connection", (socket) => {
     console.log(socket.user, "Connected");
     socket.join(socket.user);
 
     socket.on("call", (data) => {
-      console.log("Call......")
-      let calleeId = data.calleeId;
-      let rtcMessage = data.rtcMessage;
+      console.log("Call......");
+      const calleeId = data.calleeId;
+      const rtcMessage = data.rtcMessage;
 
-      socket.to(calleeId).emit("newCall", {
+      IO.to(calleeId).emit("newCall", {
         callerId: socket.user,
         rtcMessage: rtcMessage,
       });
     });
 
-    socket.on("answerCall", (data) => {
-      console.log("answerCall......")
 
-      let callerId = data.callerId;
+    socket.on("answerCall", (data) => {
+      console.log("answerCall......");
+      const callerId = data.callerId;
       rtcMessage = data.rtcMessage;
 
-      socket.to(callerId).emit("callAnswered", {
+      IO.to(callerId).emit("callAnswered", {
         callee: socket.user,
         rtcMessage: rtcMessage,
       });
@@ -43,10 +61,10 @@ module.exports.initIO = (httpServer) => {
 
     socket.on("ICEcandidate", (data) => {
       console.log("ICEcandidate data.calleeId", data.calleeId);
-      let calleeId = data.calleeId;
-      let rtcMessage = data.rtcMessage;
+      const calleeId = data.calleeId;
+      const rtcMessage = data.rtcMessage;
 
-      socket.to(calleeId).emit("ICEcandidate", {
+      IO.to(calleeId).emit("ICEcandidate", {
         sender: socket.user,
         rtcMessage: rtcMessage,
       });
@@ -54,11 +72,25 @@ module.exports.initIO = (httpServer) => {
   });
 };
 
+const verifyFirebaseToken = async (token) => {
+  console.log('Received token:', token);
+
+  try {
+    const decodedToken = await admin.auth();
+    //.verifyIdToken(token);
+    console.log("Received",token)
+    return decodedToken;
+  } catch (error) {
+    throw new Error('Firebase authentication error: ' + error.message);
+  }
+};
+
+
 module.exports.getIO = () => {
   if (!IO) {
-    console.log("IO Not Initialized")
+    console.log("IO Not Initialized");
     // throw Error("IO not initialized.");
   } else {
-    console.log("IO Initialized")
+    console.log("IO Initialized");
   }
 };
